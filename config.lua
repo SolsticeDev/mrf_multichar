a0452f31ba4da555c417be6ee73b0b98e0443737
MARFY <118571529+M4RFY@users.noreply.github.com>
2023-07-21 01:45:41 +0600

    Update config.lua

--- config.lua
+++ config.lua
@@ -7,7 +7,7 @@ Config.Interior = vector3(-1330.97, 144.38, -99.19) -- Interior to load where ch
 Config.PedCoords = vector4(-1324.92, 143.79, -99.19, 84.89) -- Create preview ped at these coordinates
 Config.HiddenCoords = vector4(-1329.06, 158.58, -99.19, 90.66) -- Hides your actual ped while you are in selection
 Config.CamCoords = vector4(-1328.57, 143.77, -99.19, 271.41) -- Camera coordinates for character preview screen
-Config.Car = vector4(-1323.73, 143.87, -99.7, 23.22) -- Car spawn only use this in a loacked interior
+Config.Car = vector4(-1323.73, 143.87, -99.7, 23.22) -- Car spawn only use this in a locked interior
 Config.CarModel = "xa21" -- Car model
 Config.EnableDeleteButton = true -- Define if the player can delete the character or not
 
